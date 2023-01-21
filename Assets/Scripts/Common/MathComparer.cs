aea98f79267ea17c73deffd00bcedaa1a4b412f3
FancyCoded <sabyrbekovjanbolot@gmail.com>
2023-01-21 13:36:59 +0600

    rafactoring

--- Assets/Scripts/Common/MathComparer.cs
+++ Assets/Scripts/Common/MathComparer.cs
@@ -3,15 +3,15 @@
     public static bool Compare(float first, float second, MathCompareType mathCompareType)
     {
         if(mathCompareType == MathCompareType.Equal)
-            if(first == second) return true;
+            return first == second;
         if(mathCompareType == MathCompareType.More)
-            if(first > second) return true;
+            return first > second;
         if (mathCompareType == MathCompareType.Less)
-            if (first < second) return true;
+            return first < second;
         if(mathCompareType == MathCompareType.EqualLess)
-            if(first <= second) return true;
+            return first <= second;
         if(mathCompareType == MathCompareType.EqualMore) 
-            if(first >= second) return true;
+            return first >= second;
 
         return false;
     }
