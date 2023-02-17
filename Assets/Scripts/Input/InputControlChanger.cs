d6467159a889789b70546f7c4bdca20084f14564
FancyCoded <sabyrbekovjanbolot@gmail.com>
2023-02-17 12:52:53 +0600

    clear

--- Assets/Scripts/Input/InputControlChanger.cs
+++ Assets/Scripts/Input/InputControlChanger.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 
 public class InputControlChanger : MonoBehaviour
